391edb81cfb84fc66e5e0001e36b07b2e3e5360e
Jonas Peter Sorensen <jonas.peter.sorensen@gmail.com>
2017-11-04 22:10:44 +0100

    hyper, bashrc and install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+echo "apt update..."
+sudo apt -qq update
+echo "✔ update done!"
+
+sudo apt -qq install zsh git
+echo "✔ base packages are installed"
+
+git pull
+echo "✔ dotfiles are up to date"
+
+ln -sf $(pwd)/bashrc ~/.bashrc
+echo "✔ zsh as default shell"
