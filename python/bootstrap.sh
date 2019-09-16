25e3e55ac7c1367cd82bf3ada58dad83f720890b
Jonas Peter Sorensen <jonas.peter.sorensen@gmail.com>
2019-09-16 16:08:34 +0200

    Updated python boostrap to use venv rather than virtualenv

--- python/bootstrap.sh
+++ python/bootstrap.sh
@@ -6,10 +6,10 @@ echo ""
 echo "Starting install of python tooling"
 echo ""
 
-echo "Python 2 stuff..."
-python2 --version
-sudo apt install python-pip
-pip --version
+#echo "Python 2 stuff..."
+#python2 --version
+#sudo apt install python-pip
+#pip --version
 
 echo ""
 echo "Python 3 stuff..."
@@ -18,8 +18,10 @@ sudo apt install python3-pip
 pip3 --version
 
 echo ""
-echo "Install virtualenv..."
-pip3 install virtualenv --user
+#echo "Install virtualenv..."
+#pip3 install virtualenv --user
+echo "Install venv..."
+sudo apt-get install -y python3-venv
 
 echo ""
 echo "Install of python tooling done."
