f667d133755a402bdf78632c739c65d7ec6cf275
Jonas Peter Sorensen <jonas.peter.sorensen@gmail.com>
2020-06-23 11:50:39 +0200

    Refactored install of azure-cli to match latest official instructions

--- azure/bootstrap.sh
+++ azure/bootstrap.sh
@@ -7,11 +7,23 @@ echo ""
 echo "Starting install of azure tooling"
 echo ""
 
+echo "Installing prerequisite packages..."
+sudo apt-get update
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+
+echo "Installing MS signing key..."
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+
+echo "Adding azure-cli repo..."
 AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-sudo tee /etc/apt/sources.list.d/azure-cli.list
-curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt-get install azure-cli 
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+echo "Installing azure-cli package..."
+sudo apt-get update
+sudo apt-get install azure-cli
 
 echo ""
 echo "Install of azure tooling done."
